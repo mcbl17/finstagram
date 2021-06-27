59b19ef7837e8146c7f875aa58fd9c6a7ee5b08d
mcbl17 <michaellawson@live.ca>
2021-06-27 21:01:31 +0000

    updated database config

--- config/database.rb
+++ config/database.rb
@@ -10,7 +10,7 @@ configure do
     database: "db/db.sqlite3"
   }
   else
-    db_url = 'postgres://[your_postgres_url here]'
+    db_url = 'postgres://whrclyekxnerpa:af1d8b28ee0b9262686a706c68da3bd9e0fc54ee802407c8fd2a288f7acc357b@ec2-34-202-54-225.compute-1.amazonaws.com:5432/d41bv2gkdjbs1f'
     db = URI.parse(ENV['DATABASE_URL'] || db_url)
     set :database {
       adapter: "postgresql",
